556171a7c439f91a4a603ed8782366c491dba4b5
João Roberto <joao.roberto@ufv.br>
2024-08-30 19:23:27 -0300

    Menu do terminal aprimorado

--- source/main.py
+++ source/main.py
@@ -1,8 +1,36 @@
 from AFD import AFD
+from MT import MT
+import sys
 
 if __name__ == "__main__":
     try:
-        automato = AFD.carregar_de_arquivo("testes/teste0.txt")
+        opçao = None
+        caminhoArquivo = None
+
+        if(sys.argv[-1] == "-g"):
+            # Insira aqui código da interface gráfica
+            print("Em breve modo gráfico")
+            sys.exit()
+
+        print("Olá, seja bem vindo, selecione qual autômato você deseja utilizar :");
+        print("1) Autômato Finito Determinístico")
+        print("2) Autômato de Pilha Determinístico")
+        print("==========Extras==========")
+        print("3)Máquina de Mealy")
+        print("4)Máquina de Moore")
+        print("5)Máquina de Turing")
+        opçao = input();
+        #caminhoArquivo = input("Insira o caminho do arquivo com a receita :")
+        #caminhoArquivo = "testes/teste0.txt"
+
+        dicionario = {
+            "1":lambda: AFD.carregar_de_arquivo("testes/teste0.txt"),
+            "2":lambda: print("Autômato de pilha ainda não implementado"),
+            "3":lambda: print("Máquina de Mealy ainda não implementada"),
+            "4":lambda: print("Máquina de Moore ainda não implementada"),
+            "5":lambda: MT.carregar_de_arquivo("testes/testeMT.txt")
+        }
+        automato = dicionario.get(opçao)();
         entrada = "apo"  # Entrada a ser processada
         aceita = automato.processar_entrada(entrada)
 
@@ -10,6 +38,7 @@ if __name__ == "__main__":
             print("Entrada aceita.")
         else:
             print("Entrada rejeitada.")
+    
     except FileNotFoundError:
         print("Erro: Arquivo não encontrado.")
     except Exception as e:
